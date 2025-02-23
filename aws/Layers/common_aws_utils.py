be040ea2f0183c089259572b498fb66824de72f6
Kesavan Chinnasamy <kesavanchinnasamy92@gmail.com>
2025-02-23 10:40:14 +0530

    Update S3Handler

--- aws/Layers/common_aws_utils.py
+++ aws/Layers/common_aws_utils.py
@@ -3,17 +3,15 @@ import pandas as pd
 import io
 class S3Handler:
     def __init__(self):
-        pass
+        s3_client = boto3.client('s3')
 
     def read_csv_from_s3(self, bucket, key):
-        s3 = boto3.client('s3')
-        obj = s3.get_object(Bucket=bucket, Key=key)
+        obj = self.s3_client.get_object(Bucket=bucket, Key=key)
         df = pd.read_csv(obj['Body'])
         return df
     
     def write_df_to_s3(self, df, bucket, key):
-        s3 = boto3.client('s3')
         csv_buffer = io.StringIO()
         df.to_csv(csv_buffer, index=False)
-        s3.put_object(Bucket=bucket, Key=key, Body=csv_buffer.getvalue())
+        self.s3_client.put_object(Bucket=bucket, Key=key, Body=csv_buffer.getvalue())
         return True
